80584c604e0fa0061f392ba46c1092fa71444350
Brian <108559046+nairbflix@users.noreply.github.com>
2024-06-08 14:41:59 +0800

    fix: move clerk checkrole to root page

--- app/(routes)/user/page.tsx
+++ app/(routes)/user/page.tsx
@@ -1,5 +1,12 @@
 import Page from "@/components/user/Page";
+import { checkRole } from "@/utils/roles";
+import { redirect } from "next/navigation";
+
 export default function Home() {
+  if (!checkRole("user")) {
+    redirect("/");
+  }
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-10">
       <Page />
